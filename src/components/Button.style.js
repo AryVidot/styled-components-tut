5ab64cd753864116bfce6a8938170705f434f736
Ary Vidot <aryvid@gmail.com>
2021-07-01 22:28:19 +0100

    first commit

---
+++ src/components/Button.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+
+export const Button = styled.button`
+    width: 200px;
+    height:50px;
+    background-color: ${(props) => props.backgroundColor};
+
+`;
+
+// export const BlueButton = styled.button`
+//     width: 200px;
+//     height:50px;
+//     background-color: blue;
+
+// `;
+// export const GreenButton = styled.button`
+//     width: 200px;
+//     height:50px;
+//     background-color: green;
+
+// `;
+// export const RedButton = styled.button`
+//     width: 200px;
+//     height:50px;
+//     background-color: red;
+
+// `;
